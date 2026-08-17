488cdefcb85d81ded07ddb58edcbeacb57f0bc16
LayiCode <olayiwolauthman310@gmail.com>
2026-08-17 16:50:28 +0100

    Update SecurityConfig.java

--- src/main/java/com/Group2/Ecommerce/Config/SecurityConfig.java
+++ src/main/java/com/Group2/Ecommerce/Config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:3000",
+                "http://localhost:5173",
                 "https://e-commerce-frontend-coral-iota.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
