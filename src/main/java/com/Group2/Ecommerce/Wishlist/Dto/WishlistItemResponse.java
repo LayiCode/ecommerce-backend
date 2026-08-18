8dc1db6282c0bd24ed57acf95e454e5108177e84
LayiCode <olayiwolauthman310@gmail.com>
2026-08-18 22:01:38 +0100

    Create WishlistItemResponse.java

---
+++ src/main/java/com/Group2/Ecommerce/Wishlist/Dto/WishlistItemResponse.java
@@ -0,0 +1,33 @@
+package com.Group2.Ecommerce.Wishlist.Dto;
+
+import com.Group2.Ecommerce.Wishlist.WishlistItem;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WishlistItemResponse {
+
+    private Long id;
+    private Long productId;
+    private String productName;
+    private BigDecimal productPrice;
+    private String productImageUrl;
+    private LocalDateTime createdAt;
+
+    public static WishlistItemResponse fromEntity(WishlistItem item) {
+        return new WishlistItemResponse(
+                item.getId(),
+                item.getProduct().getId(),
+                item.getProduct().getName(),
+                item.getProduct().getPrice(),
+                item.getProduct().getImageUrl(),
+                item.getCreatedAt()
+        );
+    }
+}
