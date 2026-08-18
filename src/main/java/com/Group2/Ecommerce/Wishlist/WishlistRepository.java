d1cd9e530af4647b0d0c6abd4eb7ef8a13a8b051
LayiCode <olayiwolauthman310@gmail.com>
2026-08-18 22:00:57 +0100

    Create WishlistRepository.java

---
+++ src/main/java/com/Group2/Ecommerce/Wishlist/WishlistRepository.java
@@ -0,0 +1,13 @@
+package com.Group2.Ecommerce.Wishlist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface WishlistRepository extends JpaRepository<WishlistItem, Long> {
+    List<WishlistItem> findByUserIdOrderByCreatedAtDesc(Long userId);
+    Optional<WishlistItem> findByUserIdAndProductId(Long userId, Long productId);
+    boolean existsByUserIdAndProductId(Long userId, Long productId);
+    void deleteByUserIdAndProductId(Long userId, Long productId);
+}
