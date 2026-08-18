6d61674c9c255ddb5d53f3656d25a116ed851a3d
LayiCode <olayiwolauthman310@gmail.com>
2026-08-18 22:01:21 +0100

    Create UpdateProfileRequest.java

---
+++ src/main/java/com/Group2/Ecommerce/User/Dto/UpdateProfileRequest.java
@@ -0,0 +1,20 @@
+package com.Group2.Ecommerce.User.Dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateProfileRequest {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email")
+    private String email;
+}
